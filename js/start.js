2ac454dbe7631e51ddcc9508f0463b66a57fc60b
betsyvies <ing.betsyvies@gmail.com>
2018-01-09 18:34:44 -0500

    añadiendo eventos a la vista login

--- js/start.js
+++ js/start.js
@@ -1,6 +1,11 @@
-/* Al dar click en el button sing up nos redireccionará a la vista form */
 $(document).ready(function() {
-    $('#sing-up').click(function() {
-      window.location.assign('info-user.html');
-    });
+  /* Al dar click en el button sing up nos redireccionará a la vista form */
+  $('#sing-up').click(function() {
+    window.location.assign('info-user.html');
   });
+  
+  /* Al dar click en el button login nos redireccionará a la vista login */
+  $('#log-in').click(function() {
+    window.location.assign('login.html');
+  });
+});
