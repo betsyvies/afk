185e60e4b73f0e288df2631464666ef3a45e70f7
franshescaqm <franshesca_94@hotmail.com>
2018-01-10 13:39:17 -0500

    cambiando sign up a register en HTML start y agregando Modals

--- js/start.js
+++ js/start.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   /* Al dar click en el button sing up nos redireccionará a la vista form */
-  $('#sing-up').click(function() {
+  /*$('#sing-up').click(function() {
     window.location.assign('info-user.html');
-  });
+  });*/
   
   /* Al dar click en el button login nos redireccionará a la vista login */
   $('#log-in').click(function() {
