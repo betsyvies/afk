5ae481770151e0ad5428813d099282d6e9cae7ab
franshescaqm <franshesca_94@hotmail.com>
2018-01-14 03:42:43 -0500

    añadiendo chat visible

--- js/plays.js
+++ js/plays.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var $name = $('#name-profile');
   var $inputPhrase = $('#input');
   var $containerPhrase = $('#container-phrase');
@@ -6,31 +6,108 @@ $(document).ready(function() {
   var $buttonIcon = $('#button-icon');
   var $buttonVideos = $('#button-videos');
   var $buttonImages = $('#button-images');
-   
+
   /* Agregamos el name con el que el usuario se registro */
   var nameValidation = window.localStorage.getItem('name');
 
   $name.text(nameValidation);
 
   /* Este evento sirve para cambiar la frase al gusto de el */
-  $inputPhrase.on('keyup', function() {
+  $inputPhrase.on('keyup', function () {
     event.preventDefault();
     var phrase = $(this).val();
     $containerPhrase.attr('data-phrase', phrase);
   });
 
-  $buttonIcon.on('click', function() {
-    var dataPhrase = $containerPhrase.attr('data-phrase'); 
+  $buttonIcon.on('click', function () {
+    var dataPhrase = $containerPhrase.attr('data-phrase');
 
     $spanPhrase.text(dataPhrase);
     $inputPhrase.val('');
   });
 
-  $buttonVideos.on('click', function() {
+  $buttonVideos.on('click', function () {
     window.location.assign('videos.html');
   });
 
-  $buttonImages.on('click', function() {
+  $buttonImages.on('click', function () {
     window.location.assign('images.html');
   });
+});
+
+//chat code
+
+$(document).on('click', '.panel-heading span.icon_minim', function (e) {
+  var $this = $(this);
+  if (!$this.hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $this.removeClass('panel-collapsed');
+    $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+  }
+});
+$(document).on('focus', '.panel-footer input.chat_input', function (e) {
+  var $this = $(this);
+  if ($('#minim_chat_window').hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $('#minim_chat_window').removeClass('panel-collapsed');
+    $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+  }
+});
+$(document).on('click', '#new_chat', function (e) {
+  var size = $(".chat-window:last-child").css("margin-left");
+  size_total = parseInt(size) + 400;
+  alert(size_total);
+  var clone = $("#chat_window_1").clone().appendTo(".container");
+  clone.css("margin-left", size_total);
+});
+$(document).on('click', '.icon_close', function (e) {
+  //$(this).parent().parent().parent().parent().remove();
+  $("#chatbox").hide();
+});
+
+// send function start
+
+function send() {
+  var chat = $("#btn-input").val();
+  var dt = new Date();
+  var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+  if (chat == "") {
+    alert('Enter Message');
+  } else {
+    var body = '<div class="row msg_container base_sent">' +
+      '<div class="col-md-10 col-xs-10 ">' +
+      '<div class="messages msg_sent">' +
+      '<p>' + chat + '</p>' +
+      ' <time datetime="2009-11-13T20:00">Gamer 1 • Today ' + time + '</time>' +
+      '</div>' +
+      '</div>' +
+      '<div class="col-md-2 col-xs-2 avatar">' +
+      '<img class="chatimg" src="../assets/images/perfil.jpg" class=" img-responsive ">' +
+      '</div>' +
+      '</div>';
+  }
+  $(body).appendTo("#messagebody");
+  $('#btn-input').val('');
+  $("#messagebody").animate({ scrollTop: $("#messagebody")[0].scrollHeight }, 'slow');
+}
+
+
+// send function end
+
+
+
+
+$("#btn-chat").click(function () {
+  send()
+});
+
+$('#btn-input').keypress(function (e) {
+  if (e.which == 13) {
+    send()
+  }
 });
